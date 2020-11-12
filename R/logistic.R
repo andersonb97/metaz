83e78fe84217ecb2eeea1d9c8b11601783e7c616
Benjamin Anderson <andersonb97@gmail.com>
2020-11-12 10:41:22 -0700

    Created package with the logistic regression function

--- R/logistic.R
+++ R/logistic.R
@@ -1,4 +1,14 @@
-# Logistic Regression Function
+#' Logistic Regression Function
+#' @param event.e is a vector containing the number of events in the treatment group
+#' @param event.c is a vector containing the number of events in the control group
+#' @param n.e is a vector containing the number of observations in the treatment group
+#' @param n.c is a vector containing the number of observations in the control group
+#' @param covar is a vector containing covariate values
+#' @import lme4
+#' @return logistic meta-regression output
+#' @export
+#' @examples
+#'
 
 logistic <- function(event.e, event.c, n.e, n.c, covar){
 
@@ -10,7 +20,7 @@ logistic <- function(event.e, event.c, n.e, n.c, covar){
 
   data.df$covar.int <- data.df$treat * data.df$covar.rep
 
-  reg.log <- lme4::glmer(cbind(event, n - event) ~ factor(study) + factor(treat) + covar.int + (treat-1|study),
+  reg.log <- glmer(cbind(event, n - event) ~ factor(study) + factor(treat) + covar.int + (treat-1|study),
                    data = data.df, family = binomial)
 
   log.results <<- unname(summary(reg.log)$coefficients[length(summary(reg.log)$coefficients[, 1]),])
